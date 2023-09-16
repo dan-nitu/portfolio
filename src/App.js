16ddd788c94255b6f7413e46f585d58465ad7aeb
Voodoo30fps <voodoo30fps@gmail.com>
2023-09-16 16:35:13 +0300

    router setup and dummy pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import './assets/scss/style.scss';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import RootLayout from './pages/Root';
+
+import HomePage from './pages/Home';
+import AboutPage from './pages/About';
+import ProjectsPage from './pages/Projects';
+import ContactPage from './pages/Contact';
+
+import ErrorPage from './pages/Error';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> },
+      {
+        path: 'about',
+        element: <AboutPage />,
+      },
+      {
+        path: 'projects',
+        element: <ProjectsPage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Root.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from 'react-router-dom';
+
+import Navigation from '../components/Navigation';
+
+const RootLayout = () => {
+  return (
+    <>
+      <Navigation />
+
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+};
+
+export default RootLayout;
