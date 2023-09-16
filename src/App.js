8c89fe71961bdba5d748c74a7e2d84f1b46a5629
Voodoo30fps <voodoo30fps@gmail.com>
2023-09-16 13:20:22 +0300

    sass structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import './assets/scss/main.scss';
+import './assets/scss/style.scss';
 
 function App() {
-  return <div className='yolo-swag'>Hello world</div>;
+  return;
 }
 
 export default App;
